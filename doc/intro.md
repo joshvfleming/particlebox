207b905f23a997bb00a69c842c823f9f8d1663bd
jfleming <jfleming@linkedin.com>
2014-01-17 12:25:03 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to physim
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/physim/core_test.clj
@@ -0,0 +1,7 @@
+(ns physim.core-test
+  (:require [clojure.test :refer :all]
+            [physim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

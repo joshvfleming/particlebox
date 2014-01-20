f84dc4788190fc9580d1d16d29ac0ca1ea5047dd
joshvfleming <jf@joshfleming.com>
2014-01-20 10:15:23 -0500

    Fix closure compilation with advanced optimizations

--- project.clj
+++ project.clj
@@ -13,5 +13,5 @@
                                    :externs ["externs/three.js"]
                                    :output-to "resources/main.js"
                                    :pretty-print false
-                                   :optimizations :simple
+                                   :optimizations :advanced
                                    }}] })

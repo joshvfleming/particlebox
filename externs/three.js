2088ff75fe52fcad29801cc44f0be622d4b396dd
jfleming <jfleming@symora.com>
2014-01-20 10:15:23 -0500

    Fix closure compilation with advanced optimizations

--- externs/three.js
+++ externs/three.js
@@ -1,1 +1,34 @@
-var THREE = {};
+var THREE = {
+  SphereGeometry: function(){},
+  MeshNormalMaterial: function(){},
+  Mesh: function(){},
+  PerspectiveCamera: function(){},
+  Scene: function(){},
+  CanvasRenderer: function(){}
+};
+
+THREE.Mesh.prototype = {
+  position: {
+    x: 0, y: 0, z: 0
+  },
+
+  rotation: {
+    x: 0, y: 0, z: 0
+  }
+};
+
+THREE.PerspectiveCamera.prototype = {
+  position: {
+    x: 0, y: 0, z: 0
+  }
+};
+
+THREE.Scene.prototype = {
+  add: function(){}
+};
+
+THREE.CanvasRenderer.prototype = {
+  setSize: function(){},
+  render: function(){},
+  domElement: null
+};
